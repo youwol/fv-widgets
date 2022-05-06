de1d4d01913ffe8d8f04c2419f6b355d7bf94bba
greinisch <greinisch@youwol.com>
2022-05-06 16:00:30 +0200

    🔧 add yw_pipeline

---
+++ .yw_pipeline/yw_pipeline.py
@@ -0,0 +1,60 @@
+from typing import List
+
+from youwol.environment.forward_declaration import YouwolEnvironment
+from youwol.environment.models import IPipelineFactory
+from youwol.environment.models_project import JsBundle, PipelineStep, FileListing, Artifact, Pipeline, Flow
+from youwol.pipelines.pipeline_typescript_weback_npm import Paths, get_dependencies
+from youwol.pipelines.publish_cdn import PublishCdnLocalStep, PublishCdnRemoteStep
+from youwol_utils.context import Context
+from youwol_utils.utils_paths import parse_json
+
+
+class BuildStep(PipelineStep):
+    id: str = "build"
+    run: str = "yarn build-css"
+    sources: FileListing = FileListing(
+        include=[Paths.package_json_file, "webpack.config.js", "src/sass"],
+        ignore=[Paths.auto_generated_file, "**/.*/*"]
+    )
+
+    artifacts: List[Artifact] = [
+        Artifact(
+            id='dist',
+            files=FileListing(
+                include=[Paths.package_json_file, "dist"]
+            ),
+            links=[]
+        )
+    ]
+
+
+class PipelineFactory(IPipelineFactory):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    async def get(self, env: YouwolEnvironment, context: Context):
+
+        async with context.start(action="pipeline") as ctx:
+            await ctx.info(text="Instantiate pipeline")
+            return Pipeline(
+                target=JsBundle(),
+                tags=["flux-view", "css", "sass"],
+                projectName=lambda path: parse_json(path / Paths.package_json_file)["name"],
+                projectVersion=lambda path: parse_json(path / Paths.package_json_file)["version"],
+                dependencies=lambda project, _ctx: get_dependencies(project),
+                steps=[
+                    BuildStep(),
+                    BuildStep(id="build-prod", run="yarn build:prod"),
+                    PublishCdnLocalStep(packagedArtifacts=['dist']),
+                    PublishCdnRemoteStep()
+                ],
+                flows=[
+                    Flow(
+                        name="prod",
+                        dag=[
+                            "build > publish-local > publish-remote "
+                        ]
+                    )
+                ]
+            )
